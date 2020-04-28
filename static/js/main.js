51488be0d93653ef5ef1c2189f248d77cecd5653
Tomporino <tomorpeter97@gmail.com>
2020-04-28 18:16:01 +0200

    haxin

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,6 @@
-// const $clothes = $('.clothes');
-//
-// function createSlots(rows, cols) {
-//     for (let i = 0; i < rows; i++) {
-//         const $row = $('<div id="cloth">').addClass('row');
-//         for (let j = 0; j < cols; j++) {
-//             const $slot = $(`<div >`).addClass('slot');
-//             $row.append($slot);
-//         }
-//         $clothes.append($row)
-//     }
-// }
-//
-// createSlots(3, 3)
+
+
+//Drag and Drop
+const cloth_slots = document.querySelectorAll('.cloth_slot')
+
 
