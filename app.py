24f1d2948fc0808155ca6a239d5bf9a2c1edee52
Tomporino <tomorpeter97@gmail.com>
2020-04-28 10:48:19 +0200

    html

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main():
+    return render_template('dressing.html')
 
 
 if __name__ == '__main__':
---
+++ static/js/dummy.js
@@ -0,0 +1,6 @@
+function get_score(attributes) {
+    
+}
+
+
+get_score({'cute': 3, 'scum': 1, 'funny':1, 'Szili': 0})
