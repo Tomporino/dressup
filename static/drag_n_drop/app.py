9f1d40501cdda0d098deb9ae088d16645126aeba
Tomporino <tomorpeter97@gmail.com>
2020-04-28 13:26:51 +0200

    drag_n_drop

--- static/drag_n_drop/app.py
+++ static/drag_n_drop/app.py
@@ -4,11 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def main():
-    return render_template('dressing.html')
+def hello_world():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(
-        debug=True
-    )
+    app.run()
---
+++ static/js/main.js
@@ -0,0 +1,17 @@
+// const $clothes = $('.clothes');
+//
+// function createSlots(rows, cols) {
+//     for (let i = 0; i < rows; i++) {
+//         const $row = $('<div id="cloth">').addClass('row');
+//         for (let j = 0; j < cols; j++) {
+//             const $slot = $(`<div >`).addClass('slot');
+//             $row.append($slot);
+//         }
+//         $clothes.append($row)
+//     }
+// }
+//
+// createSlots(3, 3)
+
+let cloth = document.createElement('img')
+cloth.src = 'static/images/clothes/cloth1.png'
