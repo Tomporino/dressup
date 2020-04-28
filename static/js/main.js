fa2a18b3f769dbb86f93ab4b6eba66260982878c
Tomporino <tomorpeter97@gmail.com>
2020-04-28 19:43:28 +0200

    change mentor head

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,34 @@
-
-
-//Drag and Drop
 const cloth_slots = document.querySelectorAll('.cloth_slot');
 const HEAD = document.querySelector('#head-image');
 const BODY = document.querySelector('#body-image');
 const LEGS = document.querySelector('#leg-image');
+const MENTORS = ['/static/images/bodies/adam2.png', '/static/images/bodies/bence2.png', '/static/images/bodies/laci2.png', '/static/images/bodies/gabor2.png'];
 
+let mentorIndex = 0;
+
+function defaultHead() {
+    document.getElementById('head-img').src = MENTORS[mentorIndex];
+}
+defaultHead();
+
+function nextMentor() {
+    if (mentorIndex < MENTORS.length - 1) {
+        mentorIndex += 1;
+    } else {
+        mentorIndex = 0
+    }
+    document.getElementById('head-img').src = MENTORS[mentorIndex]
+}
+
+
+//Drag and Drop
 //add event listeners
 
 for (const bodypart of [HEAD, BODY, LEGS]) {
     bodypart.addEventListener('dragover', dragOver)
     bodypart.addEventListener('drop', dragDrop)
     bodypart.addEventListener('dragenter', dragEnter)
-    bodypart,addEventListener('dragleave', dragLeave)
+    bodypart.addEventListener('dragleave', dragLeave)
 }
 
 for (const slot of cloth_slots) {
