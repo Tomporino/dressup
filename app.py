d5eac244fd998c88aa6b75cc489062c04c148998
Tomporino <tomorpeter97@gmail.com>
2020-04-29 11:28:20 +0200

    new way of access files

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, url_for
+from os import listdir
 
 app = Flask(__name__)
 
-# IMAGE_LIST = [['cloth1.png', 'cloth2.png', 'cloth3.png'], ['cloth4.png', 'cloth5.png', 'cloth6.png'], ['cloth7.png', 'cloth8.png', 'cloth9.png']]
-TOPS = [['adidas-tshirt-builder-top.png', 'builder-daddys-top.png', 'builder-denim-jacket-top.png'], ['gucci-pan-builder-top.png', 'mickey-gucci-builder-top.png', 'supmorty-builder-top.png']]
-BOTTOMS = [['black-sweetpants-bottom.png', 'builder-jeanshort-bottom.png', 'girl-jeans-builder'], ['grey-sweatpants-builder-bottom.png']]
-DRESSES = [['blackdress-builder.png', 'jeans-short-builder.png', 'jesus-neklace-builder.png'], ['redress-builder.png', 'whitedress-builder.png', 'yellowdress-builder.png']]
+
+TOPS = listdir('static/images/tops')
+BOTTOMS = listdir('static/images/bottoms')
+DRESSES = listdir('static/images/dresses')
 
 
 @app.route('/')
