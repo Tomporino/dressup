365841cafcb1568210f1a72a1483ead4674ef634
Tomporino <tomorpeter97@gmail.com>
2020-06-09 22:57:51 +0200

    separate clothes div from body div and canvas w h %

---
+++ static/js/canvas.js
@@ -0,0 +1,7 @@
+// let canvas = document.querySelector('canvas');
+// let ctx = canvas.getContext('2d');
+//
+// let src = document.querySelector("img")
+//
+// ctx.drawImage(src, 0, 0, 100, 100)
+console.log('loaded')
